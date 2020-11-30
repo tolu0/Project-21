5d7c2dfcfb54a537b490046fa295317882fbdf63
Tolu Opeloyeru <71344094+tolu0@users.noreply.github.com>
2020-11-30 11:48:20 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,50 @@
+var bullet,wall;
+var speed,weight;
+var thickness;
+
+function setup() {
+  createCanvas(1600,400);
+
+thickness = random(22,83);
+speed = random(223,321);
+weight = random(30,52);
+
+wall = createSprite(1200,200,thickness,height/2);
+wall.shapeColor = (80,80,80);
+
+bullet = createSprite(50,200,50,20);
+bullet.shapeColor = ("white");
+bullet.velocityX = speed;
+
+}
+
+function draw() {
+  background(0);  
+
+if(bullet.isTouching(wall)){
+
+  bullet.velocityX = 0
+  var damage = 0.5 * weight * speed * speed/(thickness * thickness * thickness);
+
+  if(damage>10){
+
+    wall.shapeColor = color(255,0,0);
+  }
+  
+  if (damage<10){
+    wall.shapeColor = color(0,255,0);
+  
+  }
+    
+}
+
+
+
+
+
+  drawSprites();
+
+
+
+}
+
